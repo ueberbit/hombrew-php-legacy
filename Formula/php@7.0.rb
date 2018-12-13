1bd4b604842a394348bf8a769c8bbb02e6a857c0
Florian Weber <florian.weber@ueberbit.de>
2018-12-13 18:09:25 +0100

    Update php@7.0.rb

--- Formula/php@7.0.rb
+++ Formula/php@7.0.rb
@@ -5,8 +5,9 @@ class PhpAT70 < Formula
   sha256 "ab8c5be6e32b1f8d032909dedaaaa4bbb1a209e519abb01a52ce3914f9a13d96"
   
   bottle do
-    root_url "https://s3-de-central.profitbricks.com/uehomebrewphplegacy"
-    sha256 "94019e1e74501ca6483e344182cba224f6495557dcaf94f80418bafce936ba16" => :mojave
+    root_url "https://homebrew.bintray.com/bottles-php-legacy"
+    rebuild 1
+    sha256 "ec31e4052d3c8d8293174a183c7f5b0a24ec443b581e4425195a20e7234de0af" => :mojave
   end
 
   keg_only :versioned_formula
